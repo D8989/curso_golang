423573c6fc027e5a8692018adb1db5b721a1eb10
Daniel J. Moreira <deljuven@ymail.com>
2019-03-01 22:34:53 -0300

    Class 08

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,13 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+Os codigos em Go sao organizados em pacotes
+e para usa-los e nescessario declarar um ou varios imports
+*/
+import "fmt"
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
