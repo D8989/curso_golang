c819bd034953bf4edbc042485b65dce7ccfd692b
Daniel J. Moreira <deljuven@ymail.com>
2019-03-04 16:59:21 -0300

    Class 15 - basic function

---
+++ fundamentos/funcoes/funcoes.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func somar(a int, b int) int { // func <nome_funcao> (<nome_parametro> <tipo_parametro> ) <tipo_retorno> { <codigo> }
+	return a + b
+}
+
+func imprimir(valor int) {
+	fmt.Println(valor)
+}
