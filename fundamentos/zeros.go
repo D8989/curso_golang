85edbab702117015bc07f0cd5fa829de63306a08
Daniel J. Moreira <deljuven@ymail.com>
2019-03-03 21:21:44 -0300

    Class 13

---
+++ fundamentos/zeros.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int     // = 0
+	var b float64 // = 0
+	var c bool    // = false
+	var d string  // = ""
+	var e *int    // = <nil>
+	fmt.Printf("%v %v %v %q %v", a, b, c, d, e)
+}
