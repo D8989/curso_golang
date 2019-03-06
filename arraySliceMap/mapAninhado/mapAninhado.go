53fb9e3f8d01ea6f3ab183b699de4bdf1fc2923b
Daniel J. Moreira <deljuven@ymail.com>
2019-03-06 00:03:23 -0300

    Class 39 - map nested

---
+++ arraySliceMap/mapAninhado/mapAninhado.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	funcsPorLetra := map[string]map[string]float64{
+		"G": {
+			"Gabriela Silva": 1545.78,
+			"Guga Pereira":   8456.78,
+		},
+		"J": {
+			"José João": 11325.45,
+		},
+		"P": {
+			"Pedro Junior": 1200.0,
+		},
+	}
+
+	delete(funcsPorLetra, "P")
+
+	for letra, funcs := range funcsPorLetra {
+		fmt.Println("Letra:", letra)
+		for nome, salario := range funcs {
+			fmt.Println("\tnome:", nome, "salario:", salario)
+		}
+	}
+}
