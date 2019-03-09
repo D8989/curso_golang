2a4af38fd2417360d6a6a4a63392997b1bb69b67
Daniel J. Moreira <deljuven@ymail.com>
2019-03-09 19:09:06 -0300

    Class 64 - reuse package

---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/D8989/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	// fmt.Println(area._TrianguloEq(5.0, 2.0)) -> não é visivel
+}
